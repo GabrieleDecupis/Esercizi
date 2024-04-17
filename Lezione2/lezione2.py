f593519f8a419bf4852c5935bc2f7d5f111b9f32
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-04-17 13:52:56 +0200

    File lezione2.py aggiunto

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,14 @@
+# Gabriele De Cupis
+# 17/04/24
+
+print("Hello World!!")
+
+#2-3. Personal Message: 
+# Use a variable to represent a person’s name, and print a message to that person. 
+# Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+name: str = "Giuseppe"
+message: str = f"ciao {name}, ti va di imparare un po' di phyton insieme?"
+print(message)
+
+#print(f"Hello {name}, would you like to learn some Python today?")
