445ea2074691213d4f16b2da47fe32864fb007b8
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-04-19 11:45:46 +0200

    Svolto fino all'esercizio 6-2

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -244,5 +244,11 @@ print(information["Age"])
 print(information["City"])
 print(          )
 
+# 6-2. Favorite Numbers: 
+# Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary. 
+# Think of a favorite number for each person, and store each as a value in your dictionary. 
+# Print each person’s name and their favorite number. 
+# For even more fun, poll a few friends and get some actual data for your program.
 
-
+print("Esercizio 6-2\n")
+#ciao a tutti
