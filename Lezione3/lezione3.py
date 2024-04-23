7e9c775c75cc91a92b95d28185c77ea44d0347c3
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-04-24 01:31:56 +0200

    5-2

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -235,8 +235,8 @@ print()
 # If you want to try more comparisons, write more tests and add them
 # to conditional_tests.py. Have at least one True and one False result for each of
 # the following:
-# • Tests for equality and inequality with strings
-# • Tests using the lower() method
+# • Tests for equality and inequality with strings OK
+# • Tests using the lower() method OK
 # • Numerical tests involving equality and inequality, greater than and less
 # than, greater than or equal to, and less than or equal to
 # • Tests using the and keyword and the or keyword
@@ -244,4 +244,45 @@ print()
 # • Test whether an item is not in a list
 
 print("Esercizio 5-2 \n")
+
+cars: list = ["Ferrari", "Audi", "Mercedes-Benz", "Volkswagen", "Aston Martin", "Bugatti", "McLaren", "Maserati", "BMW"]
+my_cars: list = ["Ferrari", "Bugatti", "Mercedes-Benz"]
+for x in cars:
+    if x in my_cars:
+        print(f"Is your car a {x}?")
+        print(f"TRUE \n")
+    else:
+        print(f"Is your car a {x}?")
+        print(f"FALSE\n")
+
+my_dream_car: str = "FERRARI"
+car2: list = ["ferrari","audi", "bmw"]
+print("Is your car a ferrari?")
+print(my_dream_car.lower() == car2[0])
+
+print()
+
+number2: list = [12, 67, 98, 100]
+number3: list = [11, 67, 100]
+for x in number2:
+    for y in number3:
+        if x == y:
+            print(f"Is {x} = or < or > {y}?")
+            print(f"Is {x} = {y}?")
+            print(f"TRUE ")
+        else:
+            print(f"Is {x} = or < or > {y}?")
+            print(f"Is {x} = {y}?")
+            print(f"FALSE")
+        if y == x:
+            print(f"{y} is = {x}")
+        elif y > x:
+            print(f"{y} is > {x}")
+        else:
+            print(f"{y} is < {x}")
+        if y >= x:
+            print(f"{y} is >= {x}\n")
+        elif y <= x:
+            print(f"{y} is <= {x}\n")
+
 
