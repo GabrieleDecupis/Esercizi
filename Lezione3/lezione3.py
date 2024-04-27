811acc4b3b2ba05646e16bd67b84a7773993920a
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-04-27 16:28:52 +0200

    5-2

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -235,11 +235,11 @@ print()
 # If you want to try more comparisons, write more tests and add them
 # to conditional_tests.py. Have at least one True and one False result for each of
 # the following:
-# • Tests for equality and inequality with strings OK
-# • Tests using the lower() method OK
+# • Tests for equality and inequality with strings
+# • Tests using the lower() method 
 # • Numerical tests involving equality and inequality, greater than and less
-# than, greater than or equal to, and less than or equal to OK
-# • Tests using the and keyword and the or keyword
+# than, greater than or equal to, and less than or equal to
+# • Tests using the and keyword and the or keyword 
 # • Test whether an item is in a list
 # • Test whether an item is not in a list
 
@@ -290,4 +290,20 @@ for x in number2:
             print(f"{y} is <= {x}\n")
 
 print()
+
+listino_macchine: list = ["Ferrari", "Audi", "Mercedes-Benz", "Volkswagen", "Aston Martin", "Bugatti", "McLaren", "Maserati", "BMW"]
+macchine_richieste: list = ["Ferrari", "Tesla", "Mercedes-Benz", "Fiat"]
+
+for auto in macchine_richieste:
+    if auto in listino_macchine and auto != "":
+        print(f"{auto}: the car is present in the list")
+    else:
+        print(f"{auto}: the car is not present in the list")
+print()
+for auto in macchine_richieste:
+    if auto in listino_macchine or auto == "":
+        print(f"{auto}: the car is present in the list")
+    else:
+        print(f"{auto}: the car is not present in the list")
+
 
