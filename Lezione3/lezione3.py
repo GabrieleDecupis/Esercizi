995dfe68e7621fd6077016f6e0251e5ec9a0cf05
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-04-29 18:14:04 +0200

    Completati esercizi lezione 3

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -306,4 +306,183 @@ for auto in macchine_richieste:
     else:
         print(f"{auto}: the car is not present in the list")
 
+print()
 
+# 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. 
+# Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# • Write an if statement to test whether the alien’s color is green. If it is, print a message 
+# that the player just earned 5 points.
+# • Write one version of this program that passes the if test and another that fails. 
+# (The version that fails will have no output.)
+
+print("Esercizio 5-3\n")
+
+alien_color = input("choose a color between green, yellow, red: ")
+if alien_color == "green":
+    print(f"You shot a {alien_color} alien: you earn 5 POINTS")
+else:
+    pass
+
+print()
+
+# 5-4. Alien Colors #2: 
+# Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+# • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+# • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+# • Write one version of this program that runs the if block and another that runs the else block.
+
+print("Esercizio 5-4\n")
+
+alien_color2 = ["green", "yellow", "red"]
+for x in alien_color2:
+    if x == "green":
+        print(f"You shoot a {x} alien: you earn 5 POINTS")
+    else:
+        print(f"You shoot a {x} alien: you earn 10 POINTS")
+
+print()
+
+# 5-5. Alien Colors #3:
+# Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+# • If the alien is green, print a message that the player earned 5 points.
+# • If the alien is yellow, print a message that the player earned 10 points.
+# • If the alien is red, print a message that the player earned 15 points.
+# • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+print("Esercizio 5-5\n")
+
+alien_color2 = ["green", "yellow", "red"]
+for x in alien_color2:
+    if x == "green":
+        print(f"You shoot a {x} alien: you earn 5 POINTS")
+    elif x == "yellow":
+        print(f"You shoot a {x} alien: you earn 10 POINTS")
+    else:
+        print(f"You shoot a {x} alien: you earn 15 POINTS")
+
+print()
+
+# 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. 
+# Set a value for the variable age, and then:
+# • If the person is less than 2 years old, print a message that the person is a baby.
+# • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+# • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# • If the person is age 65 or older, print a message that the person is an elder.
+
+print("Esercizio 5-6\n")
+
+x = input("Insert your age: ")
+person_age = int(x)      # altrimenti potevo scrivere x: int = int(input("insert your age: "))
+if person_age < 2:
+    print(f"You are a baby!")
+elif 2 <= person_age < 4:
+    print("You are a toddler")
+elif 4 <= person_age < 13:
+    print("You are a kid")
+elif 13 <= person_age < 20:
+    print("You are a teenager")
+elif 20 <= person_age < 65:
+    print("You are an adult")
+else:
+    print("You are elder")
+
+print()
+
+# 5-7. Favorite Fruit: 
+# Make a list of your favorite fruits, and then write a series of independent if statements that check 
+# for certain fruits in your list.
+# • Make a list of your three favorite fruits and call it favorite_fruits.
+# • Write five if statements. Each should check whether a certain kind of fruit is in your list. 
+# If the fruit is in your list, the if block should print a statement, such as You really like Apples!
+
+favourite_fruits: list = ["Strawberry", "Cherry", "Watermelon"]
+x = input("Choose a fruit, (first letter must be capital): ")
+if x in favourite_fruits:
+    print(f"I really like {x} too")
+if x not in favourite_fruits:
+    print(f"I don't like {x}")
+
+print()
+
+# 5-8. Hello Admin: 
+# Make a list of five or more usernames, including the name 'admin'. 
+# Imagine you are writing code that will print a greeting to each user after they log in to a website. 
+# Loop through the list, and print a greeting to each user.
+# • If the username is 'admin', print a special greeting, 
+#   such as Hello admin, would you like to see a status report?
+# • Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.
+
+print("Esercizio 5-8\n")
+
+user_list: list = ["admin", " gabriele", "pippo", "pluto", "topolino"]
+for x in user_list:
+    if x == "admin":
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {x}, thank you for logging in again")
+
+print()
+
+# 5-9. No Users: 
+# Add an if test to hello_admin.py to make sure the list of users is not empty.
+# • If the list is empty, print the message We need to find some users!
+# • Remove all of the usernames from your list, and make sure the correct message is printed.
+
+print("Esercizio 5-9\n")
+
+user_list_empty: list = []
+if user_list_empty ==[]:
+    print(f"We need to find some users!")
+x = input("Please, insert a new user: ")
+user_list_empty.append(x)
+print(user_list_empty)
+
+print()
+
+# 5-10. Checking Usernames: 
+# Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+# • Make a list of five or more usernames called current_users.
+# • Make another list of five usernames called new_users. 
+#   Make sure one or two of the new usernames are also in the current_users list.
+# • Loop through the new_users list to see if each new username has already been used. 
+#   If it has, print a message that the person will need to enter a new username. 
+#   If a username has not been used, print a message saying that the username is available.
+# • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. 
+#   (To do this, you’ll need to make a copy of current_users containing the lowercase versions 
+#   of all existing users.)
+
+print("Esercizio 5-10\n")
+
+current_users: list = ['Eragon', 'Gabriele', 'Admin', 'Astrid', 'Laika']
+new_users: list = ['eragon', 'gabriele', 'Pippo', 'Pluto', 'Paperina']
+current_users_2: list = [lower_name.lower() for lower_name in current_users]
+for x in new_users:
+        if x.lower() in current_users_2:
+            print(f"{x} is already used, plese choose another one!")
+        else:
+            print(f"Your username is available!")
+
+print()
+
+# 5-11. Ordinal Numbers: 
+# Ordinal numbers indicate their position in a list, such as 1st or 2nd. 
+# Most ordinal numbers end in th, except 1, 2, and 3.
+# • Store the numbers 1 through 9 in a list.
+# • Loop through the list.
+# • Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. 
+#   Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+
+print("Esercizio 5-11\n")
+
+ordinal_numbers: list = [x for x in range(1,10)]
+for x in ordinal_numbers:
+    if x == 1:
+        print("1st")
+    elif x == 2:
+        print("2nd")
+    elif x ==3:
+         print("3rd")
+    else:
+         print(f"{x}th")
