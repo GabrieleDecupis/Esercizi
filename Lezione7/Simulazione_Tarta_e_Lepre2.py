7e78d2e3d41d92ff666592256b3644cb03bc67f3
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-05-15 13:44:25 +0200

    Empty

---
+++ Lezione7/Simulazione_Tarta_e_Lepre2.py
@@ -0,0 +1,13 @@
+# SFIDE AGGIUNTIVE:
+# 
+# 1. Variabilità Ambientale:
+# 
+# Introdurre fattori ambientali che possono influenzare la corsa, come il meteo.
+# Ad esempio, la pioggia può ridurre la velocità di avanzamento o aumentare la probabilità di scivolate per entrambi i concorrenti. 
+# Implementare un sistema dove le condizioni 'soleggiato' e 'pioggia' cambiano dinamicamente ogni 10 tick dell'orologio.
+#  
+# Modificatori mossa:
+# 
+# - La Tartaruga in caso di pioggia subisce penalità -1 su ogni mossa. In caso di sole non subisce variazioni.
+# - La Lepre in caso di pioggia subisca una penalità -2 su ogni mossa. In caso di sole non subisce variazioni.
+
