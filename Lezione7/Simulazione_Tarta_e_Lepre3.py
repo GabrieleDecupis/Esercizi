00b5173b8d600b8a6f7798a024331ba7f9b1aa52
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-05-15 13:44:43 +0200

    Empty

---
+++ Lezione7/Simulazione_Tarta_e_Lepre3.py
@@ -0,0 +1,23 @@
+# SFIDE AGGIUNTIVE:
+
+# Energia o Stamina:
+
+# Aggiungere una metrica di "energia" o "stamina" che diminuisce con ogni movimento e si ricarica in specifiche condizioni. 
+# Fare in modo che le mosse che consumano più energia non possano essere eseguite se l'animale non ha abbastanza energia. 
+# L'energia inziale per entrambi gli animali è 100.
+
+# Nuove regole di movimento:
+
+# - Tartaruga:
+#     - Per la tartaruga, ogni volta che il numero generato indica una mossa ma non è possibile eseguirla per mancanza di energia, 
+#       essa guadagna 10 di energia.
+#     - Passo veloce (50% di probabilità): avanza di 3 quadrati e richiede 5 di energia.
+#     - Scivolata (20% di probabilità): arretra di 6 quadrati e richiede 10 di energia. Non può andare sotto il quadrato 1.
+#     - Passo lento (30% di probabilità): avanza di 1 quadrato e richiede 3 di energia.
+
+# - Lepre:
+#     - Riposo (20% di probabilità): non si muove e recupera 10 di energia. Non può superare l'energiza iniziale.
+#     - Grande balzo (20% di probabilità): avanza di 9 quadrati  e richiede 15 di energia.
+#     - Grande scivolata (10% di probabilità): arretra di 12 quadrati e richiede 20 di energia. Non può andare sotto il quadrato 1.
+#     - Piccolo balzo (30% di probabilità): avanza di 1 quadrato e richiede 5 di energia.
+#     - Piccola scivolata (20% di probabilità): arretra di 2 quadrati e richiede 8 di energia. Non può andare sotto il quadrato 1.
