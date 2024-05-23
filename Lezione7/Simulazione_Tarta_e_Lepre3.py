99aabadeb87e4c03fc5805e87bc5335b3a838c7c
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-05-23 18:51:09 +0200

    Completato

--- Lezione7/Simulazione_Tarta_e_Lepre3.py
+++ Lezione7/Simulazione_Tarta_e_Lepre3.py
@@ -25,33 +25,14 @@
 
 import random
 
+posizione_L: int = 0
 posizione_T: int = 0
 round: int = 1
-
 
 #valori stamina tartaruga
 
 stamina_tarta: int = 100
-
-
-def tartaruga() -> int:
-    x = random.randrange(1, 11)
-    if x <= 5:
-        return 3
-    elif 6 <= x <= 7:
-        return -6
-    else:
-        return +1
-
-def energie_tarta(move_tarta: int) -> int:
-    if move_tarta == 3:
-        stamina_round = 5  # modifica stamina_round in stamina_tarta_round
-    elif move_tarta == -6:
-        stamina_round = 10
-    else:
-        stamina_round = 3
-    
-    return stamina_round
+stamina_lepre: int = 100
 
 def lepre() -> int:
     x = random.randrange(1, 11)
@@ -65,56 +46,112 @@ def lepre() -> int:
         return 1
     elif 9 <= x <= 10:
         return -2
-    
+
+def tartaruga() -> int:
+    x = random.randrange(1, 11)
+    if x <= 5:
+        return 3
+    elif 6 <= x <= 7:
+        return -6
+    else:
+        return +1
+
 def energie_lepre(move_lepre: int) -> int:
     if move_lepre == 0:
-        stamina_round
+        stamina_round = -10  # modifica stamina_round in stamina_lepre_round
+    elif move_lepre == 9:
+        stamina_round = 15
+    elif move_lepre == -12:
+        stamina_round = 20
+    elif move_lepre == 1:
+        stamina_round = 5
+    else:
+        stamina_round = 8
+    
+    return stamina_round
 
+def energie_tarta(move_tarta: int) -> int:
+    if move_tarta == 3:
+        stamina_round = 5  # modifica stamina_round in stamina_tarta_round
+    elif move_tarta == -6:
+        stamina_round = 10
+    else:
+        stamina_round = 3
+    
+    return stamina_round
 
-
-def posizione(tarta: int) -> list:
+def posizione(tarta:int, lepre:int):
     circuito: list = []
     lunghezza_lista = 70
     circuito = ['_'] * lunghezza_lista
+
     if tarta < 0:
         tarta = 0
     elif tarta >= 70:
         tarta = 69
     
     circuito[tarta] = "T"
+
+    if lepre < 0:
+        lepre = 0
+    elif lepre >= 70:
+        lepre = 69
+    
+    circuito[lepre] = "H"
+
+    if tarta == lepre:
+        circuito[lepre] = "OUCH"
+    
     return circuito
-
-
-
 
 while posizione_T <= 69:
     print()
     print(f"Round {round}\n")
     move_tarta = tartaruga()
-    stamina_round = energie_tarta(move_tarta)
+    move_lepre = lepre()
+    stamina_round_tarta = energie_tarta(move_tarta)
+    stamina_round_lepre = energie_lepre(move_lepre)
     
-    stamina_tarta -= stamina_round
+    stamina_tarta -= stamina_round_tarta
     stamina_usata_tarta: int = 100 - stamina_tarta
-
+    stamina_lepre -= stamina_round_lepre
+    stamina_usata_lepre: int = 100 - stamina_lepre
+    
     if stamina_tarta < 0:
         move_tarta = 0
         stamina_tarta += 10
         print(" tarta no energie")
     
+    if stamina_lepre < 0:
+        move_lepre = 0
+        stamina_lepre += 10
+        print(" lepre no energie")
+    elif stamina_lepre > 100:
+        stamina_lepre = 100
+    
     posizione_T += move_tarta
+    posizione_L += move_lepre
 
-    gara = posizione(posizione_T)
+    gara = posizione(posizione_T, posizione_L)
 
     lista_stamina = ["|"] * stamina_tarta
-    lista_stamina_usata = ["_"] * stamina_usata_tarta
+    lista_stamina_usata_tarta = ["_"] * stamina_usata_tarta
+    print()
+
+    lista_stamina = ["|"] * stamina_lepre
+    lista_stamina_usata_lepre = ["_"] * stamina_usata_lepre
     print()
     
     print()
     print(" ".join(gara))
     print()
-    print("STAMINA TARTA:  " + "".join(lista_stamina) +" " + " ".join(lista_stamina_usata) + f" {stamina_tarta}/100" )
+    print("STAMINA TARTA:  " + "".join(lista_stamina) +" " + " ".join(lista_stamina_usata_tarta) + f" {stamina_tarta}/100\n" )
+    print("STAMINA LEPRE:  " + "".join(lista_stamina) +" " + " ".join(lista_stamina_usata_lepre) + f" {stamina_lepre}/100\n" )
     round += 1
 
-    if posizione_T == 69:
-        print("Tarta win")
-        break
+    if posizione_T >= 69:
+        print("Tarta win!!")
+    elif posizione_L >= 69:
+        print("Lepre win!!")
+    elif posizione_L >= 69 and posizione_T >= 69:
+        print("IT'S A TIE BUT TORTOISE WINS! || VAY!!!")
