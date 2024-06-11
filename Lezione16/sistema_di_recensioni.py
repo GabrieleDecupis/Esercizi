dab13e48c7437f5275c274d7727173121d4f2355
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-06-11 19:17:35 +0200

    Completato

--- Lezione16/sistema_di_recensioni.py
+++ Lezione16/sistema_di_recensioni.py
@@ -32,4 +32,84 @@
 # valutazioni e richiami il metodo recensione().
 
 class Media:
-    def __init__ (self, )
+    def __init__ (self, title: str):
+        self.title = title
+        self.reviews: list[int] = []
+    
+    def set_title(self, title: str):
+        self.title = title
+
+    def get_title(self):
+        return f"Titolo del film: {self.title}"
+    
+    def aggiungiValutazione(self, voto: int):
+        if 1<= voto <= 5:
+            self.reviews.append(voto)
+    
+    def getMedia(self):
+        return round(sum(x for x in self.reviews) / len(self.reviews), 1)
+    
+    def getRate(self):
+        x = Media.getMedia(self)
+        if x <= 2:
+            return "Giudizio: Terribile"
+        if 2 < x <= 3:
+            return "Giudizio: Brutto"
+        if 3 < x <= 4:
+            return "Giudizio: Normale"
+        if 4 < x < 4.5:
+            return "Giudizio: Bello"
+        else:
+            return "Giudizio: Grandioso"
+    
+    def ratePercentage(self, voto: int):
+        counter = 0
+        for x in self.reviews:
+            if x == voto:
+                counter += 1
+        
+        percentuale = round((counter * 100) / len(self.reviews), 1)
+        if voto == 1:
+            return f"Terribile: {percentuale}%"
+        if voto == 2:
+            return f"Brutto: {percentuale}%"
+        if voto == 3:
+            return f"Normale: {percentuale}%" 
+        if voto == 4:
+            return f"Bello: {percentuale}%"
+        if voto == 5:
+            return f"Grandioso: {percentuale}%"
+
+    def recensione(self):
+        return f"{Media.get_title(self)}\nVoto Medio: {Media.getMedia(self)}\n{Media.getRate(self)}\n{Media.ratePercentage(self, 1)}\n{Media.ratePercentage(self, 2)}\n{Media.ratePercentage(self, 3)}\n{Media.ratePercentage(self, 4)}\n{Media.ratePercentage(self, 5)}"
+
+
+# film1 = Media("il pianeta del tesoro")
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(5)
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(4)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# film1.aggiungiValutazione(3)
+# print(film1.recensione())
