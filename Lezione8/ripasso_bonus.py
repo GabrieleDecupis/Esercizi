678700c0f5106ed64d8a70ab464ed801efda8cfe
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-06-28 13:47:51 +0200

    non ce la farò mai! =)

---
+++ Lezione8/ripasso_bonus.py
@@ -0,0 +1,15 @@
+# Esercizio 1
+
+# The Number of Beautiful Subsets: write a function with an array nums of positive integers and a positive integer k given as inputs. 
+# A subset of nums is beautiful if it does not contain two integers with an absolute difference equal to k. Return the number of non-empty 
+# beautiful subsets of the array nums. A subset of nums is an array that can be obtained by deleting some (possibly none) elements from nums. 
+# Two subsets are different if and only if the chosen indices to delete are different.
+
+# Example 1:
+# Input: nums = [2,4,6], k = 2
+# Output: 4
+
+# Example 2:
+# Input: nums = [1], k = 1
+# Output: 1
+
