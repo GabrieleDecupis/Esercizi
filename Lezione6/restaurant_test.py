c854a84ed68548130e37c77296a16f9e731998a3
GabrieleDecupis <gabrieledecupis1997@gmail.com>
2024-07-10 18:22:00 +0200

    .

---
+++ Lezione6/restaurant_test.py
@@ -0,0 +1,9 @@
+from restaurant import Restaurant
+
+x = Restaurant("Ciccio", "Italiana")
+x.describe_restaurant()
+x.open_restaurant()
+x.set_number_served(100)
+x.describe_restaurant()
+x.increment_number_served(10000000)
+x.describe_restaurant()
